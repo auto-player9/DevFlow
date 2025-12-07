726f9f70ade0f722f8eb56d42e64e42c2bcc6c06
auto-player9 <168700454+auto-player9@users.noreply.github.com>
2025-12-07 16:21:47 +0200

    Update conditional rendering for sign-in state

--- components/navigation/Sidebar.tsx
+++ components/navigation/Sidebar.tsx
@@ -47,7 +47,7 @@ async function AppSidebar() {
                                     <NavLinks isMobileNav/>
                                 </section>
                             </SidebarMenu>
-                            {!isSignedIn && <div className="flex flex-col gap-3">
+                            {!session && <div className="flex flex-col gap-3">
                                 <div>
                                     <Link href={Routes.SIGN_IN}>
                                         <Button
