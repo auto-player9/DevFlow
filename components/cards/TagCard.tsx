784219f772cf7b0792467cf3dc3e67a11469e19f
saleh haydar <salehhaydar@gamil.com>
2026-01-14 16:42:06 +0200

    Display tags

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -1,7 +1,7 @@
 import ROUTES from "@/constants/routes";
 import Link from "next/link";
 import {Badge} from "@/components/ui/badge";
-import {getDeviconClassName} from "@/lib/utils";
+import {cn, getDeviconClassName, getTechDescription} from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
 
@@ -13,7 +13,7 @@ interface Props {
     showCount?: boolean;
     compact?: boolean;
     remove?: boolean;
-    isButton: boolean;
+    isButton?: boolean;
     handleRemoveQuestion?: () => void;
 }
 
@@ -23,6 +23,7 @@ const handleClick = (e: React.MouseEvent) => {
 
 function TagCard({_id, name, questions, showCount, compact, remove, isButton, handleRemoveQuestion}: Props) {
     const iconClass = getDeviconClassName(name)
+    const iconDescreption = getTechDescription(name)
 
     const content = (
         <>
@@ -62,6 +63,29 @@ function TagCard({_id, name, questions, showCount, compact, remove, isButton, ha
         )
     }
 
+
+    return (
+        <Link href={ROUTES.TAG(_id)} className="shadow-light100_darknone">
+            <article className="light-border flex w-full flex-col rounded-2xl border px-8 py-10 sm:w-[260px]">
+                <div className="flex items-center justify-between gap-3">
+                    <div className="w-fit rounded-sm px-5 py-1.5">
+                        <p className="paragraph-semibold text-dark300_light900">{name}</p>
+                    </div>
+                    <i className={cn(iconClass, "text-2xl")} aria-hidden="true"/>
+                </div>
+                <p className="small-regular text-dark500_light700 mt-5 line-clmap-3 w-full">
+                    {iconDescreption}
+                </p>
+
+                <p className="small-medium text-dark400_light500 mt-3.5">
+                    <span className="body-semibold primary-text-gradient mr-2.5">
+                        {questions}+
+                    </span>
+                    Questions
+                </p>
+            </article>
+        </Link>
+    )
 
 }
 
--- components/search/LocalSearch.tsx
+++ components/search/LocalSearch.tsx
@@ -55,13 +55,13 @@ function LocalSearch({route, imgSrc, placeholder, otherClasses, iconPosition = "
             <div
                 className="background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4">
                 {iconPosition === "left" && (
-                    <Image src="/icons/search.svg" width={24} height={24} alt="Serach" className="cursor-pointer"/>
+                    <Image src={imgSrc} width={24} height={24} alt="Serach" className="cursor-pointer"/>
                 )}
-                <Input type="text" placeholder="Search..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}
+                <Input type="text" placeholder={placeholder? placeholder : "Search..."} value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}
                        className="paragraph-regular no-focus placeholder text-dark400_light700 border-none shadow-none outline-none"
                 />
                 {iconPosition === "right" && (
-                    <Image src="/icons/search.svg" width={24} height={24} alt="Serach" className="cursor-pointer"/>
+                    <Image src={imgSrc} width={24} height={24} alt="Serach" className="cursor-pointer"/>
                 )}
             </div>
         </>
